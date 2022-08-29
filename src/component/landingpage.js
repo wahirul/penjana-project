59a1166aace3dd42302ae4830b954d28f8d7edaa
wahirul <wahirularif@gmail.com>
2022-08-29 15:46:58 +0800

    second commit

--- src/component/landingpage.js
+++ src/component/landingpage.js
@@ -8,6 +8,7 @@ export const Landing = () =>{
         <div className='center'>
             <p className = 'font-bold text-2xl text-center text-red-600'>Landing Page</p>
             <button className="border-2" class="btn btn-red" onClick= {() => navigate('/myprofile')}>Go to My profile</button>
+            <p>Add something</p>
         </div>
     )
 }
